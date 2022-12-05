b44ca37fd20ad4418afc8875abf67400585f24ba
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2022-12-05 02:34:15 -0500

    refactored the codes, included even listener and click incrementation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:last-child');
+let index1 = 0;
+let index2 = 0;
+let index3 = 0;
 
 
 let Image = function(name, src){
@@ -13,7 +16,7 @@ let Image = function(name, src){
   this.views = 0;
 };
 
-let bag = new Image('bag', './img/bag.jpg,');
+let bag = new Image('bag', './img/bag.jpg');
 let banana = new Image('banana', './img/banana.jpg');
 let bathroom = new Image('bathroom', './img/bathroom.jpg');
 let boots = new Image('boot', './img/boots.jpg');
@@ -44,22 +47,73 @@ function randomImageIndex(){
 // render function: invoke function on page load, I want to render 3 random images
 function renderImages(){
   for (let i = 0; i < images.concat.length; i++){
-    let firstImage = images[randomImageIndex()];
-    let secondImage = images[randomImageIndex()];
-    let thirdImage = images[randomImageIndex()];
+    index1 = randomImageIndex();
+    index2 = randomImageIndex();
+    index3 = randomImageIndex();
+
+    // make the random images be different. we use a while loop instead of for loop because a for loop will keep looping once the statement is true. we want it to move on once true.
+    while (index1 === index2 || index2 === index3 || index1 === index3) {
+      index1 = randomImageIndex();
+      index2 = randomImageIndex();
+      index3 = randomImageIndex();
+    }
+
+    let firstImage = images[index1];
+    let secondImage = images[index2];
+    let thirdImage = images[index3];
+
     // DOM Manipulation
     image1.src = firstImage.src;
+    image1.alt = firstImage.name;
+    image1.title = firstImage.name;
     image2.src = secondImage.src;
+    image2.alt = secondImage.name;
+    image2.title = secondImage.name;
     image3.src = thirdImage.src;
+    image3.alt = thirdImage.name;
+    image3.title = thirdImage.name;
+
+    // increwment views
+    firstImage.views++;
+    secondImage.views++;
+    thirdImage.views++;
+  }
+}
+
+// even handler
+// what happens when our viewers click an image?
+  // increment image .clicks
+  // render 3 new images
+function handleImageclick(event) {
+  // the event object knows about the event, and the element targeted
+  console.log(event.target);
+
+  // how to increment the correct image clicked
+  // method to iterate over the image array
+  // 1. iterate over images
+  // 2. if images[i].alt == event.target
+  // another method:
+  // use global index variables
+  if (images[index1].name === event.target.name) {
+    images[image1].clicks++;
+  }
+  if (images[index2].name === event.target.name) {
+    images[image2].clicks++;
+  }
+  if (images[index3].name === event.target.name) {
+    images[image3].clicks++;
   }
 
-
+  console.log(images);
+  renderImages();
 }
 
 // render on page load
+image1.addEventListener('click', handleImageclick);
+image2.addEventListener('click', handleImageclick);
+image3.addEventListener('click', handleImageclick);
 renderImages();
 
-// even handler
 
 
 
